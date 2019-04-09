a70de7eece3f0d6d6a23058d5e95809de40a100e
Teerapong Singthong <st.teerapong@gmail.com>
2019-04-09 22:01:20 +0700

    feat: add context and combine multiple span

---
+++ example/01_trace_hello._world.go
@@ -0,0 +1,39 @@
+/*
+	That single span combined two operations performed by the program,
+	formatting the output string and printing it.
+*/
+
+package example
+
+import (
+	"fmt"
+	"os"
+
+	tracing "github.com/iamgoangle/go-http-tracing-kafka/internal/tracing"
+	"github.com/opentracing/opentracing-go/log"
+)
+
+func HelloWorld() {
+	if len(os.Args) != 2 {
+		panic("ERROR: Expecting one argument")
+	}
+
+	tracer, closer := tracing.Init("hello-world")
+	defer closer.Close()
+
+	helloTo := os.Args[1]
+
+	span := tracer.StartSpan("say-hello")
+	span.SetTag("hello-to", helloTo)
+
+	helloStr := fmt.Sprintf("Hello, %s!", helloTo)
+	span.LogFields(
+		log.String("event", "string-format"),
+		log.String("value", helloStr),
+	)
+
+	println(helloStr)
+	span.LogKV("event", "println")
+
+	span.Finish()
+}
--- go.mod
+++ go.mod
@@ -9,4 +9,7 @@ require (
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/uber/jaeger-client-go v2.16.0+incompatible
 	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
+	go.uber.org/atomic v1.3.2 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.9.1 // indirect
 )
